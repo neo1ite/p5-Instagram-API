cdfd8d6aebd66701a74dad3bbaa78241c9141f88
Neo1ite <neo1515@gmail.com>
2017-01-30 13:20:53 +0300

    Add Endpoints and tests

---
+++ lib/Instagram/API/Endpoints.pm
@@ -0,0 +1,120 @@
+package Instagram::API::Endpoints;
+
+use utf8;
+use strict;
+use warnings;
+use autouse 'Data::Dumper';
+
+use URI::Escape;
+
+use constant BASE_URL                           => 'https://www.instagram.com';
+use constant ACCOUNT_PAGE                       => 'https://www.instagram.com/{username}';
+use constant MEDIA_LINK                         => 'https://www.instagram.com/p/{code}';
+use constant ACCOUNT_MEDIAS                     => 'https://www.instagram.com/{username}/media?max_id={max_id}';
+use constant ACCOUNT_JSON_INFO                  => 'https://www.instagram.com/{username}/?__a=1';
+use constant MEDIA_JSON_INFO                    => 'https://www.instagram.com/p/{code}/?__a=1';
+use constant MEDIA_JSON_BY_LOCATION_ID          => 'https://www.instagram.com/explore/locations/{{facebookLocationId}}/?__a=1&max_id={{maxId}}';
+use constant MEDIA_JSON_BY_TAG                  => 'https://www.instagram.com/explore/tags/{tag}/?__a=1&max_id={max_id}';
+use constant GENERAL_SEARCH                     => 'https://www.instagram.com/web/search/topsearch/?query={query}';
+use constant ACCOUNT_JSON_INFO_BY_ID            => 'ig_user({userId}){id,username,external_url,full_name,profile_pic_url,biography,followed_by{count},follows{count},media{count},is_private,is_verified}';
+use constant LAST_COMMENTS_BY_CODE              => 'ig_shortcode({{code}}){comments.last({{count}}){count,nodes{id,created_at,text,user{id,profile_pic_url,username,follows{count},followed_by{count},biography,full_name,media{count},is_private,external_url,is_verified}},page_info}}';
+use constant COMMENTS_BEFORE_COMMENT_ID_BY_CODE => 'ig_shortcode({{code}}){comments.before({{commentId}},{{count}}){count,nodes{id,created_at,text,user{id,profile_pic_url,username,follows{count},followed_by{count},biography,full_name,media{count},is_private,external_url,is_verified}},page_info}}';
+use constant LAST_LIKES_BY_CODE                 => 'ig_shortcode({{code}}){likes{nodes{id,user{id,profile_pic_url,username,follows{count},followed_by{count},biography,full_name,media{count},is_private,external_url,is_verified}},page_info}}';
+
+use constant INSTAGRAM_QUERY_URL                => 'https://www.instagram.com/query/';
+use constant INSTAGRAM_CDN_URL                  => 'https://scontent.cdninstagram.com/';
+
+sub getAccountPageLink
+{
+    my ($username) = @_;
+
+    return ACCOUNT_PAGE =~ s/\{username\}/uri_escape_utf8($username)/ser;
+}
+
+sub getAccountJsonLink
+{
+    my ($username) = @_;
+
+    return ACCOUNT_JSON_INFO =~ s/\{username\}/uri_escape_utf8($username)/ser;
+}
+
+sub getAccountJsonInfoLinkByAccountId
+{
+    my ($id) = @_;
+
+    return ACCOUNT_JSON_INFO_BY_ID =~ s/\{userId\}/uri_escape($id)/ser;
+}
+
+sub getAccountMediasJsonLink
+{
+    my ($username, $maxId) = @_;
+    $maxId //= '';
+
+    return (ACCOUNT_MEDIAS =~ s/\{username\}/uri_escape_utf8($username)/ser)
+        =~ s/\{max_id\}/uri_escape_utf8($maxId)/ser;
+}
+
+sub getMediaPageLink
+{
+    my ($code) = @_;
+
+    return MEDIA_LINK =~ s/\{code\}/uri_escape_utf8($code)/ser;
+}
+
+sub getMediaJsonLink
+{
+    my ($code) = @_;
+
+    return MEDIA_JSON_INFO =~ s/\{code\}/uri_escape_utf8($code)/ser;
+}
+
+sub getMediasJsonByLocationIdLink
+{
+    my ($facebookLocationId, $maxId) = @_;
+    $maxId //= '';
+
+    return (MEDIA_JSON_BY_LOCATION_ID =~ s/\{\{facebookLocationId\}\}/uri_escape_utf8($facebookLocationId)/ser)
+        =~ s/\{\{maxId\}\}/uri_escape_utf8($maxId)/ser;
+}
+
+sub getMediasJsonByTagLink
+{
+    my ($tag, $maxId) = @_;
+    $maxId //= '';
+
+    return (MEDIA_JSON_BY_TAG =~ s/\{tag\}/uri_escape_utf8($tag)/ser)
+        =~ s/\{max_id\}/uri_escape_utf8($maxId)/ser;
+}
+
+sub getGeneralSearchJsonLink
+{
+    my ($query) = @_;
+
+    return GENERAL_SEARCH =~ s/\{query\}/uri_escape_utf8($query)/ser;
+}
+
+sub getLastCommentsByCodeLink
+{
+    my ($code, $count) = @_;
+
+    return (LAST_COMMENTS_BY_CODE =~ s/\{\{code\}\}/uri_escape_utf8($code)/ser)
+        =~ s/\{\{count\}\}/uri_escape_utf8($count)/ser;
+}
+
+sub getCommentsBeforeCommentIdByCode
+{
+    my ($code, $count, $commentId) = @_;
+
+    return ((COMMENTS_BEFORE_COMMENT_ID_BY_CODE =~ s/\{\{code\}\}/uri_escape_utf8($code)/ser)
+        =~ s/\{\{count\}\}/uri_escape_utf8($count)/ser)
+            =~ s/\{\{commentId\}\}/uri_escape_utf8($commentId)/ser;
+}
+
+sub getLastLikesByCodeLink
+{
+    my ($code) = @_;
+
+    return LAST_LIKES_BY_CODE =~ s/\{\{code\}\}/uri_escape_utf8($code)/ser;
+}
+
+1;
---
+++ t/02_Instagram-API-Endpoints.t
@@ -0,0 +1,14 @@
+use utf8;
+use strict;
+use warnings;
+
+use Test::More qw(no_plan);
+BEGIN { use_ok('Instagram::API::Endpoints') };
+
+is(Instagram::API::Endpoints::getAccountPageLink('test_name'), 'https://www.instagram.com/test_name');
+is(Instagram::API::Endpoints::getAccountPageLink('тестовое_имя'), 'https://www.instagram.com/%D1%82%D0%B5%D1%81%D1%82%D0%BE%D0%B2%D0%BE%D0%B5_%D0%B8%D0%BC%D1%8F');
+
+is(Instagram::API::Endpoints::getAccountJsonLink('test_user'), 'https://www.instagram.com/test_user/?__a=1');
+is(Instagram::API::Endpoints::getAccountJsonLink('тестовый_пользователь'), 'https://www.instagram.com/%D1%82%D0%B5%D1%81%D1%82%D0%BE%D0%B2%D1%8B%D0%B9_%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C/?__a=1');
+
+is(Instagram::API::Endpoints::getAccountJsonInfoLinkByAccountId('1234567890_0987654321'), 'ig_user(1234567890_0987654321){id,username,external_url,full_name,profile_pic_url,biography,followed_by{count},follows{count},media{count},is_private,is_verified}');
